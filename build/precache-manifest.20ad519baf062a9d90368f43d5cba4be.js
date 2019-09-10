00a086c48689b739a30ee9611bf669f7f59ea017
Daneil Greaves <daneil@itspixelperfect.com>
2019-09-10 12:51:08 -0500

    Remove build folder from .gitignore

---
+++ build/precache-manifest.20ad519baf062a9d90368f43d5cba4be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b496c9a0b57c1901c9005f9e0209bf95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efa8aa1551662b481ef6",
+    "url": "/static/css/main.607ac0f5.chunk.css"
+  },
+  {
+    "revision": "2a8a5c02ca1b1b4f9e56",
+    "url": "/static/js/2.3f85877c.chunk.js"
+  },
+  {
+    "revision": "efa8aa1551662b481ef6",
+    "url": "/static/js/main.1d65b171.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
