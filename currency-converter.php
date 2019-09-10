9e55372eda5bfbd80ac37127201026ad1de15d4b
Daneil Greaves <daneil@itspixelperfect.com>
2019-09-10 13:04:06 -0500

    update packages

--- currency-converter.php
+++ currency-converter.php
@@ -16,6 +16,19 @@ function wp_currency_converter_show_converter()
 add_action('wp_enqueue_scripts', 'wp_currency_converter_enqueue_scripts');
 function wp_currency_converter_enqueue_scripts()
 {
+    wp_enqueue_script('react', 'https://unpkg.com/react@16/umd/react.production.min.js', '', null, false);
+    wp_enqueue_script('react-dom', 'https://unpkg.com/react-dom@16/umd/react-dom.production.min.js', '', null, false);
+    wp_enqueue_script('babel', 'https://npmcdn.com/babel-core@5.8.38/browser.min.js', '', null, false);
     wp_enqueue_script('wp-currency-converter', plugin_dir_url(__FILE__) . 'build/static/js/main.1d65b171.chunk.js');
     wp_enqueue_style('wp-currency-converter', plugin_dir_url(__FILE__) . 'build/static/css/main.607ac0f5.chunk.css');
+}
+// Add "babel" type to converter script
+add_filter('script_loader_tag', 'wp_currency_converter_add_babel_type', 10, 3);
+function wp_currency_converter_add_babel_type($tag, $handle, $src)
+{
+    if ($handle !== 'wp-currency-converter') {
+        return $tag;
+    }
+
+    return '<script src="' . $src . '" type="text/babel"></script>' . "\n";
 }
