253384eb50f9d3986ce8b2208ed8ae32ba69e5b2
Daneil Greaves <daneil@itspixelperfect.com>
2019-09-10 13:36:58 -0500

    update currency-converter.php

--- currency-converter.php
+++ currency-converter.php
@@ -20,8 +20,8 @@ function wp_currency_converter_enqueue_scripts()
     wp_enqueue_script('react-dom', 'https://unpkg.com/react-dom@16/umd/react-dom.production.min.js', '', null, false);
     wp_enqueue_script('babel', 'https://npmcdn.com/babel-core@5.8.38/browser.min.js', '', null, false);
     wp_enqueue_script('wp-currency-converter', plugin_dir_url(__FILE__) . 'build/static/js/main.1d65b171.chunk.js');
-    // wp_enqueue_script('wp-currency-converter', plugin_dir_url(__FILE__) . 'build/static/js/2.3f85877c.chunk.js');
-    // wp_enqueue_script('wp-currency-converter', plugin_dir_url(__FILE__) . 'build/static/js/runtime~main.a8a9905a.js');
+    wp_enqueue_script('wp-currency-converter', plugin_dir_url(__FILE__) . 'build/static/js/2.3f85877c.chunk.js');
+    wp_enqueue_script('wp-currency-converter', plugin_dir_url(__FILE__) . 'build/static/js/runtime~main.a8a9905a.js');
     wp_enqueue_style('wp-currency-converter', plugin_dir_url(__FILE__) . 'build/static/css/main.607ac0f5.chunk.css');
 }
 // Add "babel" type to converter script
